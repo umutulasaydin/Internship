c4e1516b04cd08155572313c8bb463278343aa84
Umut Ulaş Aydın <umutulas@sabanciuniv.edu>
2023-07-27 15:25:17 +0300

    Database migration

---
+++ Week1/loginAPI/loginAPI/Migrations/20230727105000_DatabaseInitialize.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace loginAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabaseInitialize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    username = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    refreshToken = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    expiryTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.username);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
