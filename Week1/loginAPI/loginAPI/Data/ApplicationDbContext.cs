bf0f9185d0205fa4a837138187e03c506fb75734
Umut Ulaş Aydın <umutulas@sabanciuniv.edu>
2023-07-27 15:23:57 +0300

    Database connection

---
+++ Week1/loginAPI/loginAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using loginAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace loginAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Week1/loginAPI/loginAPI/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace loginAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public string username { get; set; }
+        public string password { get; set; }
+
+        public string refreshToken { get; set; }
+
+        public DateTime expiryTime { get; set; }
+    }
+
+    public class UserLogin
+    {
+        [Key]
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+
+    public class Tokens
+    {
+        public string accessToken { get; set; }
+        public string refreshToken { get; set; }
+    }
+}
