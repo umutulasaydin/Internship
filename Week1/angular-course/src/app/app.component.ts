ca4165283fca016a2d7b17abe0928500d0e11477
Umut Ulaş Aydın <umutulas@sabanciuniv.edu>
2023-07-28 15:09:22 +0300

    Angular Basics

---
+++ Week1/angular-course/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {Course} from './model/course';
+import {Observable} from 'rxjs';
+import { CoursesService } from './services/courses.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  courses$ : Observable<Course[]>;
+  
+
+  constructor(private coursesService: CoursesService) {
+
+  }
+
+  ngOnInit() {
+
+    this.courses$ = this.coursesService.loadCourses();
+  }
+
+  save(course:Course) {
+    this.coursesService.saveCourse(course).subscribe(
+      () => console.log("Course saved")
+    );
+  }
+
+
+}
