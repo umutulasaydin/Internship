67437ec442e90b4b14c6330dfb6284eefc1f0d27
Umut Ulaş Aydın <umutulas@sabanciuniv.edu>
2023-09-01 17:07:35 +0300

    web services added

--- Week6/CouponManagementServiceFrontend/src/app/services/web/web.service.ts
+++ Week6/CouponManagementServiceFrontend/src/app/services/web/web.service.ts
@@ -7,16 +7,39 @@ import { Observable } from 'rxjs';
 })
 export class WebService {
   apiUrl: string = "https://localhost:7247/api/";
-
+  body: object = {
+    "clientName":"string",
+    "clientPos":"string"
+  };
   constructor(private http: HttpClient) { }
 
   SignUp(post: any): Observable<any>
   {
-    return this.http.post<any>(`${this.apiUrl}Auth/Signup`, post, {observe: 'response'});
+    return this.http.post<any>(`${this.apiUrl}Auth/Signup`, Object.assign(this.body,post), {observe: 'response'});
   }
 
   Login(post: any): Observable<any>
   {
-    return this.http.post<any>(`${this.apiUrl}Auth/Login`, post);
+    return this.http.post<any>(`${this.apiUrl}Auth/Login`, Object.assign(this.body,post));
+  }
+
+  UserInfo(post: any): Observable<any>
+  {
+    return this.http.post<any>(`${this.apiUrl}Coupon/GetUserInfo`, Object.assign(this.body,post));
+  }
+
+  Dashboard(post: any): Observable<any>
+  {
+    return this.http.post<any>(`${this.apiUrl}Coupon/Dashboard`, Object.assign(this.body,post));
+  }
+
+  CreateCoupon(post: any): Observable<any>
+  {
+    return this.http.post<any>(`${this.apiUrl}Coupon/Create`, Object.assign(this.body,post));
+  }
+
+  GetAllCoupons(post: any): Observable<any>
+  {
+    return this.http.post<any>(`${this.apiUrl}Coupon/GetAllCoupons`, Object.assign(this.body, post));
   }
 }
